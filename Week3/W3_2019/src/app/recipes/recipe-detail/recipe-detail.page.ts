2d8195c267e2aba95c46f534ce9d918ec97ea3fd
Yehezkiel Gunawan <yehezkiel.gunawan@student.umn.ac.id>
2019-08-28 11:24:13 +0700

    Submit Week3

---
+++ Week3/W3_2019/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -0,0 +1,50 @@
+import { create } from 'domain';
+import { AlertController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RecipesService } from '../recipes.service';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.page.html',
+  styleUrls: ['./recipe-detail.page.scss'],
+})
+export class RecipeDetailPage implements OnInit {
+  loadedRecipe: Recipe;
+  constructor(private activatedRoute: ActivatedRoute, private recipesSvc:
+    RecipesService, private router: Router, public alertController: AlertController) { }
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(
+      paramMap => {
+        if (!paramMap.has('recipeId')) { return; }
+        this.loadedRecipe = this.recipesSvc.getRecipe(paramMap.get('recipeId'));
+      }
+    );
+  }
+
+  deleteRecipe(){
+    this.recipesSvc.deleteRecipe(this.loadedRecipe.id);
+    this.router.navigate(['/recipes']);
+  }
+
+  async presentAlert(){
+    const alert = await this.alertController.create({
+      header: 'Delete Recipe?',
+      message: 'Are you sure you want to delete this recipe?',
+      buttons:[
+        {
+          text: 'YES',
+          handler: () => this.deleteRecipe()
+        },
+        {
+          text: 'Cancel',
+          role: 'Cancel'
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+}
