7b82985dd530696967bb8d3ee4edadd113cfdd59
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2019-10-31 17:18:52 +0700

    Added android capacitor for week10

--- Week10/toDoList/src/app/home/home.page.ts
+++ Week10/toDoList/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Todo, TodoService } from './../services/todo.service';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
   todos: Todo[];
+  private updateSubscription: Subscription;
 
   constructor(private todoService: TodoService) { }
 
   ngOnInit() {
-    this.todoService.getTodos().subscribe(res => {
-      this.todos = res;
-    });
+    this.updateSubscription = this.todoService.getTodos().subscribe(res => {
+                            this.todos = res;
+                          });
   }
 
-  remove(item){
+  remove(item) {
     this.todoService.removeTodo(item.id);
   }
 }
